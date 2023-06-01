e22e2d9075153e3ae3ba8a1738eb0f3fca76aeb7
Kapil Chaudhary <kc685838@gmail.com>
2023-06-01 22:28:22 +0530

    submitting code-Thu Jun 01 2023 22:28:20 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,43 @@ import './../styles/App.css';
 import Modal from "./Modal";
 
 const App = () => {
-  const [show, setShow] = useState(false);
+  // const [show, setShow] = useState(false);
 
-  function onClose() {
-    setShow(false);
-  }
+  // function onClose() {
+  //   setShow(false);
+  // }
+
+  // window.addEventListener("click", (event) => {
+  //   if(event.target !== Modal){
+  //     setShow(false);
+  //   }
+  // }) 
+
+  // return (
+  //   <div className="main">
+  //     <button onClick={() => {setShow(true)}}>Show Modal</button>
+  //     <Modal showProp={show} setShowProp={onClose} />
+  //   </div>
+  // )
+  const [showModal, setShowModal] = useState(false);
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
 
   return (
-    <div className="main">
-      <button onClick={() => {setShow(true)}}>Show Modal</button>
-      <Modal showProp={show} setShowProp={onClose} />
+    <div className='modal'>
+      <button id='open' onClick={() => setShowModal(true)}>Open Modal</button>
+      <Modal show={showModal} onClose={handleCloseModal}>
+        {/* Content for the modal */}
+        <div className='modal'>
+        <h2>Modal Title</h2>
+        <p>This is the content of the modal.</p>
+        </div>
+
+      </Modal>
     </div>
-  )
+  );
 }
 
 export default App
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-const Modal = (prop) => {
-    const { showProp, setShowProp } = prop;
+const Modal = ({ show, onClose, children }) => {
+    const modalRef = useRef(null);
+
+    const handleCloseModal = (event) => {
+        if (event.target === modalRef.current) {
+            onClose();
+        }
+    };
+
+    if (!show) {
+        return null;
+    }
+
     return (
-        <>
-            {showProp && <div className="modal-overlay">
-                <button className='modal-close' onClick={() => {
-                    setShowProp(false);
-                }}>Close</button>
-                <p>This is the content of the modal.</p>
-            </div>}
-        </>
-    )
-}
+        <div className="modal-overlay" onClick={handleCloseModal} ref={modalRef}>
+            <div className="modal-dialog" onClick={onClose}>
+                <button className="modal-close" onClick={onClose}>
+                    Close
+                </button>
+                <div className="modal-content">{children}</div>
+            </div>
+        </div>
+    );
+};
 
-export default Modal
+export default Modal;
+
+// import React from 'react';
+
+// const Modal = (prop) => {
+//     const { showProp, setShowProp } = prop;
+//     return (
+//         <>
+//             {showProp &&
+//             <div className="modal-overlay">
+//                 <div className="modal">
+//                     <button className='modal-close' onClick={() => {
+//                         setShowProp(false);
+//                     }}>Close</button>
+//                     <p>This is the content of the modal.</p>
+//                 </div>
+//             </div>}
+//         </>
+//     )
+// }
+
+// export default Modal
